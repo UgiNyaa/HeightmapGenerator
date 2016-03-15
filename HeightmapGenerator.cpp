87b9d2003f59153f58bff29dd7a81f52a048dc42
Hydro844 <hydro844@cock.li>
2016-03-15 15:35:03 +0100

    nearly ready to compile

--- HeightmapGenerator.cpp
+++ HeightmapGenerator.cpp
@@ -1,14 +1,10 @@
 #include "HeightmapGenerator.h"
 #include "Heightmap.cpp"
+#include <cstdlib>
+#include <ctime>
 
 #ifndef heightmapgeneratorcpp
 #define heightmapgeneratorcpp
-
-struct Point{
-	int x;
-	int y;
-	Point(int x, int y){this->x=x;this->y=y;}
-};
 
 HeightmapGenerator::HeightmapGenerator(){}
 
@@ -17,7 +13,7 @@ Heightmap HeightmapGenerator::Generate(int width, int height){
 	Point points[width * height];
 	
 	for (int i = 0; i < (width * height); i++){
-		points[i] = -1;
+		points[i] = Point(-1,-1);
 	}
 	
 	srand(time(0));
@@ -27,54 +23,35 @@ Heightmap HeightmapGenerator::Generate(int width, int height){
 	heightmap.setPixel(0,       height-1, rand() % 256);
 	heightmap.setPixel(width-1, height-1, rand() % 256);
 	
-	definedChars[0]                              = height.getPixel(0,       0);
-	definedChars[width-1]                        = height.getPixel(width-1, 0);
-	definedChars[width * (height-1)]             = height.getPixel(0,       height-1);
-	definedChars[width * (height-1) + (width-1)] = height.getPixel(width-1, height-1);
+	points[0]                              = Point(0, 0);                             
+	points[width-1]                        = Point(width-1, 0);
+	points[width * (height-1)]             = Point(0, height-1);
+	points[width * (height-1) + (width-1)] = Point(width-1, height-1);
 	
-	diamond(heightmap, definedChars, width-1, height-1, width, height);
+	diamond(heightmap, points, width-1, height-1, width, height);
 	}
 
-void diamond(Heightmap heightmap, Point points, int sizex, int sizey, int width, int height){
+void Heightmap::diamond(Heightmap heightmap, Point points, int sizex, int sizey, int width, int height){
 	int hsx = sizex / 2;
 	int hsy = sizey / 2;
 	
-	for (int i = 0; i < points.Length; i++)
+	for (int i = 0; i < (sizeof(points)/sizeof(points[0])); i++)
 	{
+		if (point.x != -1 || point.y != -1)
+			continue;
+		
+		// generate the 4 points to be filled
 		Point point = points[i];
 		Point a(point.x - hsx, point.y - hsy);
 		Point b(point.x + hsx, point.y - hsy);
 		Point c(point.x - hsx, point.y + hsy);
 		Point d(point.x + hsx, point.y + hsy);
+
+		// generate the 4 values of colour the points will have
+		int valuea, valueb, valuec, valued;
+		gen4valdia(a,b,c,d,hsx,hsy,valuea,valueb,valuec,valued);
 		
-		int valuea = (
-		(heightmap.getPixel(a.x - hsx, a.y - hsy))+
-		(heightmap.getPixel(a.x + hsx, a.y - hsy))+
-		(heightmap.getPixel(a.x - hsx, a.y + hsy))+
-		(heightmap.getPixel(a.x + hsx, a.y + hsy))
-		) / 4;
-		
-		int valueb = (
-		(heightmap.getPixel(b.x - hsx, b.y - hsy))+
-		(heightmap.getPixel(b.x + hsx, b.y - hsy))+
-		(heightmap.getPixel(b.x - hsx, b.y + hsy))+
-		(heightmap.getPixel(b.x + hsx, b.y + hsy))
-		) / 4;
-		
-		int valuec = (
-		(heightmap.getPixel(c.x - hsx, c.y - hsy))+
-		(heightmap.getPixel(c.x + hsx, c.y - hsy))+
-		(heightmap.getPixel(c.x - hsx, c.y + hsy))+
-		(heightmap.getPixel(c.x + hsx, c.y + hsy))
-		) / 4;
-		
-		int valued = (
-		(heightmap.getPixel(d.x - hsx, d.y - hsy))+
-		(heightmap.getPixel(d.x + hsx, d.y - hsy))+
-		(heightmap.getPixel(d.x - hsx, d.y + hsy))+
-		(heightmap.getPixel(d.x + hsx, d.y + hsy))
-		) / 4;
-		
+		// if our calculated points does not exist, just skip them
 		if (a.x >= 0 && a.x < width &&
 		    a.y >= 0 && a.x < height)
 			heightmap.setPoint(a.x, a.y, valuea);
@@ -92,15 +69,143 @@ void diamond(Heightmap heightmap, Point points, int sizex, int sizey, int width,
 			heightmap.setPoint(d.x, d.y, valuea);
 	}
 	
+	//after doing the diamond part, do the square part
 	square(heightmap, points, sizex, sizey, width, height);
 }
 
-void square(Heightmap heightmap, Point points, int sizex, int sizey, int width, int height){
+void Heightmap::square(Heightmap heightmap, Point points, int sizex, int sizey, int width, int height){
+
+	int hsx = sizex / 2;
+	int hsy = sizey / 2;
 	
+	for (int i = 0; i < points.Length; i++)
+	{
+		// generate the 4 points to be filled
+		Point point = points[i];
+		Point a(point.x, point.y - hsy);
+		Point b(point.x + hsx, point.y);
+		Point c(point.x - hsx, point.y);
+		Point d(point.x, point.y + hsy);
+
+		// generate the 4 values of colour the points will have
+		int valuea, valueb, valuec, valued;
+		gen4valsqr(a,b,c,d,hsx,hsy,valuea,valueb,valuec,valued);
+		
+		// if our calculated points does not exist, just skip them
+		if (a.x >= 0 && a.x < width &&
+		    a.y >= 0 && a.x < height)
+			heightmap.setPoint(a.x, a.y, valuea);
+		
+		if (b.x >= 0 && b.x < width &&
+		    b.y >= 0 && b.x < height)
+			heightmap.setPoint(b.x, b.y, valueb);
+		
+		if (c.x >= 0 && c.x < width &&
+		    c.y >= 0 && c.x < height)
+			heightmap.setPoint(c.x, c.y, valuec);
+		
+		if (d.x >= 0 && d.x < width &&
+		    d.y >= 0 && d.x < height)
+			heightmap.setPoint(d.x, d.y, valuea);
+	}
+	
+	
+	if (sizex >= 1 &&& sizey >= 1)
+		return;
 	
 	sizex = sizex / 2;
 	sizey   = sizey / 2;
 	diamond(heightmap, points, sizex, sizey, width, height);
+}
+
+int myavg(int a, int b, int c, int d){
+	int count = 4;
+	
+	if (a == -1){
+		a = 0;
+		count --;
+	}
+	
+	if (b == -1){
+		b = 0;
+		count --;
+	}
+	
+	if (c == -1){
+		c = 0;
+		count --;
+	}
+	
+	if (d == -1){
+		d = 0;
+		count --;
+	}
+	
+	return (a+b+c+d)/count;
+}
+
+void gen4valdia (Point a, Point b, Point c, Point d, 
+              int hsx, int hsy, 
+			  int& valuea, int& valueb, int& valuec, int& valued){
+	valuea = myavg(
+	(heightmap.getPixel(a.x - hsx, a.y - hsy)),
+	(heightmap.getPixel(a.x + hsx, a.y - hsy)),
+	(heightmap.getPixel(a.x - hsx, a.y + hsy)),
+	(heightmap.getPixel(a.x + hsx, a.y + hsy))
+	);
+		
+	valueb = myavg(
+	(heightmap.getPixel(b.x - hsx, b.y - hsy)),
+	(heightmap.getPixel(b.x + hsx, b.y - hsy)),
+	(heightmap.getPixel(b.x - hsx, b.y + hsy)),
+	(heightmap.getPixel(b.x + hsx, b.y + hsy))
+	);
+		
+	valuec = myavg(
+	(heightmap.getPixel(c.x - hsx, c.y - hsy)),
+	(heightmap.getPixel(c.x + hsx, c.y - hsy)),
+	(heightmap.getPixel(c.x - hsx, c.y + hsy)),
+	(heightmap.getPixel(c.x + hsx, c.y + hsy))
+	);
+		
+	valued = myavg(
+	(heightmap.getPixel(d.x - hsx, d.y - hsy)),
+	(heightmap.getPixel(d.x + hsx, d.y - hsy)),
+	(heightmap.getPixel(d.x - hsx, d.y + hsy)),
+	(heightmap.getPixel(d.x + hsx, d.y + hsy))
+	);	  
+}
+
+void gen4valsqr (Point a, Point b, Point c, Point d, 
+              int hsx, int hsy, 
+			  int& valuea, int& valueb, int& valuec, int& valued){
+	valuea = myavg(
+	(heightmap.getPixel(a.x, a.y - hsy)),
+	(heightmap.getPixel(a.x + hsx, a.y)),
+	(heightmap.getPixel(a.x - hsx, a.y)),
+	(heightmap.getPixel(a.x, a.y + hsy))
+	);
+		
+	valueb = myavg(
+	(heightmap.getPixel(b.x, b.y - hsy)),
+	(heightmap.getPixel(b.x + hsx, b.y)),
+	(heightmap.getPixel(b.x - hsx, b.y)),
+	(heightmap.getPixel(b.x, b.y + hsy))
+	);
+		
+	valuec = myavg(
+	(heightmap.getPixel(c.x, c.y - hsy)),
+	(heightmap.getPixel(c.x + hsx, c.y)),
+	(heightmap.getPixel(c.x - hsx, c.y)),
+	(heightmap.getPixel(c.x, c.y + hsy))
+	);
+		
+	valued = myavg(
+	(heightmap.getPixel(d.x, d.y - hsy)),
+	(heightmap.getPixel(d.x + hsx, d.y)),
+	(heightmap.getPixel(d.x - hsx, d.y)),
+	(heightmap.getPixel(d.x, d.y + hsy))
+	);	  
 }
 
 #endif
--- HeightmapGenerator.h
+++ HeightmapGenerator.h
@@ -3,13 +3,22 @@
 #ifndef heightmapgeneratorh
 #define heightmapgeneratorh
 
+struct Point{
+	int x;
+	int y;
+	Point();
+	Point(int x, int y){this->x=x;this->y=y;}
+};
+
 class HeightmapGenerator{
 	private:
+	void diamond(Heightmap heightmap, Point points, int sizex, int sizey, int width, int height);
+	void square(Heightmap heightmap, Point points, int sizex, int sizey, int width, int height);
 	
 	public:
 	HeightmapGenerator();
-	Heightmap Generate();
+	Heightmap Generate(int width, int height);
 	
-}
+};
 
 #endif
